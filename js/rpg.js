ed429323986aa0b9034d09aa8d2064c0f7d3e515
AtylaDaSilva <atylamendes97@gmail.com>
2022-07-11 16:29:45 -0300

    added comments

--- js/rpg.js
+++ js/rpg.js
@@ -1,3 +1,4 @@
+// generatePassword() is invoked when generate password button is clicked
 function generatePassword() {
     var password = "";
     
@@ -20,23 +21,31 @@ function generatePassword() {
         return;
     }
 
+    //Verifies if the "Starts with" button has been pressed...
     const startsWith = document.querySelector("input#start").checked;
 
+    //if so, startWith is attached to the password string
     if (startsWith) {
         password = attachStart(password);
     }
 
-    //Gets a character from charPool and concatenates it to password string
+    //Generates a pair of indexes to obtain a character from charPool. The character is then concatenated in 
+    //the password string
     for (var i = length; i > 0; i--) {
         var index1 = generateIndex(charPool.length);
 
-
+        //checkContains() will return -1 if no options inside the "Contanis" field are checked...
         let noOptionsSelected = checkContains(index1);
+
+        //and render the message inside renderPassword().
         if (noOptionsSelected === -1) {
             renderPassword("No Options Selected");
             return;
         }
         
+        //checkContains() checks if the number generated in index1 is "compatible" with the options selected
+        //in the "Contains" field. This is done to prevent characters that haven't been selected in the "Contains"
+        //field from being generated
         while (!checkContains(index1)) {
             
             index1 = generateIndex(charPool.length);
@@ -48,8 +57,10 @@ function generatePassword() {
         password += charPool[index1][index2];
     }
 
+    //Checks if the "Ends with" checkbox has been checked...
     const endsWith = document.querySelector("input#end").checked;
 
+    //if so, attaches the value from the "Ends with" to the password string
     if (endsWith) {
         password = attachEnd(password);
     }
@@ -58,15 +69,20 @@ function generatePassword() {
     renderPassword(password);
 };
 
+//Gets the value from the "Length" input field
 function getPasswordLength() {
     return parseInt(document.querySelector("#length").value);
 };
 
+//Displays password inside the "Result" input field
 function renderPassword(password) {
     const resultField = document.querySelector("#result");
     resultField.value = password;
 };
 
+
+//Generates a pseudo-random integer from 0 to range -1. The generated number will be used as index for the
+//charPool array
 function generateIndex(range) {
     if (range < 1) {
         return 0;
@@ -75,18 +91,23 @@ function generateIndex(range) {
     return Math.floor(Math.random() * range);
 };
 
+//Attaches the value from the "Starts with" field to the start of the password string
 function attachStart(password) {
     const startText = document.querySelector("#start-text").value;
 
     return startText + password;
 };
 
+//Attaches the value from the "Ends with" field to the end of the password string
 function attachEnd(password) {
     const endText = document.querySelector("#end-text").value;
 
     return password + endText;
 };
 
+//Checks if index is contains within the selected options inside the "Contains" checkbox field.
+//Returns true if index is equal to one of the selected options from "Contains". Otherwise, checkContains() returns
+//false
 function checkContains(index) {
     //Checkbox list references
     const checkboxList = document.querySelectorAll("#checkbox-list > li > input");
@@ -101,12 +122,13 @@ function checkContains(index) {
         checkedChars[i] = checkboxList[i].dataset.index;
     }
 
+    //Verifies every element inside the checkedChars array. If all alements are undefined, noCheckedChars will
+    //be true.
     let noCheckedChars = checkedChars.every((element) => {
         element === undefined;
     })
 
-    console.log(noCheckedChars);
-
+    //If noCheckedChars is true, the user has not selected any options in the "Contains" field.
     if (noCheckedChars) {
         return -1;
     }
@@ -121,6 +143,7 @@ function checkContains(index) {
 
 };
 
+//Copies the value from the "Result" field to the clipboard
 function copyToClipboard() {
     const passwordField = document.querySelector("#result");
     const password = passwordField.value;
@@ -132,6 +155,7 @@ function copyToClipboard() {
     alterCopyButtonTitle("Copied!");
 };
 
+//Alters the title HTML attribute from the copy to clipboard button
 function alterCopyButtonTitle(newTitle) {
     const button = document.querySelector("#copy-button");
     button.title = newTitle;
