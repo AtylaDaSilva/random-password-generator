c034520298fe609e3c92f66454fd28cdcb35aa6e
AtylaDaSilva <atylamendes97@gmail.com>
2022-07-20 22:26:46 -0300

    adjusted and imrpoved copy button

--- js/rpg-bs.js
+++ js/rpg-bs.js
@@ -6,6 +6,9 @@ lengthInput.addEventListener("input", handleLengthInput);
 //Generate password button
 const submit = document.querySelector("#submitButton");
 submit.addEventListener("click", generatePassword);
+//Copy to clipboard button
+const copyButton = document.querySelector("#copyButton");
+copyButton.addEventListener("click", copyToClipboard);
 
 //Functions
 function loadPage() {
@@ -99,8 +102,6 @@ function generateIndex(range) {
     return Math.floor(Math.random() * range);
 };
 
-//+++++++++++++++++ TO DO ++++++++++++++++++++
-
 //Checks if index is contains within the selected options inside the "Contains" checkbox field.
 //Returns true if index is equal to one of the selected options from "Contains". Otherwise, checkContains() returns
 //false
@@ -140,8 +141,6 @@ function checkContains(index) {
 
 };
 
-//+++++++++++++++++ TO DO ++++++++++++++++++++
-
 //Attaches the value from the "Starts with" field to the start of the password string
 function attachStart(password) {
     const startText = document.querySelector("#startsWith").value;
@@ -155,3 +154,39 @@ function attachEnd(password) {
 
     return password + endText;
 };
+
+//Copies the value from the "Result" field to the clipboard
+function copyToClipboard() {
+    const passwordField = document.querySelector("#result");
+    const password = passwordField.value;
+
+    passwordField.select();
+
+    navigator.clipboard.writeText(password);
+
+    copyButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="42" fill="currentColor" class="bi bi-clipboard-check"
+        viewBox="0 0 16 16">
+        <path fill-rule="evenodd"
+            d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+        <path
+            d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+        <path
+            d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+        </svg>
+    `;
+
+    let timer = setTimeout(revertCopyButtonIcon, 3000);
+};
+
+function revertCopyButtonIcon() {
+    copyButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="42" fill="currentColor" class="bi bi-clipboard"
+        viewBox="0 0 16 16">
+        <path
+            d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+        <path
+            d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+        </svg>
+    `;
+}
