9a308e69a07db1b2bcb0a1d36b6c167dec9f60f3
Atyla da Silva <atylamendes97@gmail.com>
2023-11-17 17:34:35 -0300

    Reimported CRA framework
    Reimported Create React App framework

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-//React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-//Components
-import App from './components/App';
-
-//CSS
-import './css/index.css';
-
-//WebVitals
-import reportWebVitals from './tests/reportWebVitals';
-
-//App
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
