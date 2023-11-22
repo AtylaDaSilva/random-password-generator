280ad78bf63e439df639b954ea0e3e1304f7e8ce
Atyla da Silva <atylamendes97@gmail.com>
2023-11-21 21:04:51 -0300

    Implemented password generation logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,78 @@ import { React, useState } from 'react';
 import '../css/App.css';
 
 export default function App() {
+  /**
+   * Generates a pseudo-random password by pulling characters from a characted pool
+   * @returns The generated password string
+   */
+  function generatePassword() {
+    const charPool = [
+      //Numbers
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      //Symbols
+      ["!", "?", "@", "#", "$", "%", "&", "*", "(", ")", "[", "]", "{", "}", "-", "_", "=", "+", "§", "'", '"', "`", "~", "^", ",", "<", ">", ".", ";", ":", "/", "\\", "|"],
+      //Lower Case Letters
+      ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+      //Upper Case Letters
+      ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    ];
+
+    //Password Form Data
+    const startsWith = formData.startsWith;
+    const endsWith = formData.endsWith;
+    const passwordLength = formData.passwordLength;
+    const options = Object.values(formData.options);
+    let password = "";
+
+    //Check if at least 1 option was selected
+    const noOptionsSelected = options.every(opt => opt === false);
+
+    if (noOptionsSelected) return password;
+
+    //Generate password
+    for (let i = 0; i < passwordLength; i++) { 
+      password += getRandomChar(charPool, options);
+    }
+
+    //Return concatenated password
+    return `${startsWith}${password}${endsWith}`;
+  }
+
+  /**
+   * Returns a pseudo-random element from a charPool matrix. The second argument is an array of booleans that is equivalent to what options the user selected in the password form.
+   * @param {*} charPool A bi-dimensional matrix
+   * @param {*} options An array of booleans
+   * @returns A pseudo-ranom element from the charPool matrix
+   */
+  function getRandomChar(charPool, options) { 
+    //Generate the first index of the matrix
+    let index1 = "";
+
+    //Make sure the element in the options array at the position of index1 is true.
+    do { 
+      index1 = Math.floor(Math.random() * charPool.length);
+    } while (options[index1] == false)
+
+    //Generate the second index of the matrix
+    const index2 = Math.floor(Math.random() * charPool[index1].length);
+
+    //Return an element from charPool
+    return charPool[index1][index2];
+  }
+
+  /**
+   * Updates state with the generated password when the submit button is clicked
+   * @param {*} event Event object passed by the onChange prop
+   */
   function handleSubmit(event) { 
     event.preventDefault();
-    return;
+    
+    setFormData(currentFormData => { 
+      return {
+        ...currentFormData,
+        result: generatePassword()
+      };
+    });
   }
 
   /**
