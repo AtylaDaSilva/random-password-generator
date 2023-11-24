125fce1262202777b614da52c9f2b64678b3cfba
Atyla da Silva <atylamendes97@gmail.com>
2023-11-24 15:19:41 -0300

    Adjustments to header & border
    Set header font size to a fixed value.  Made the main container border round.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -125,10 +125,10 @@ export default function App(props) {
       </div>
       <div
         id='main-container'
-        className="container border border-primary d-flex flex-column justify-content-start mt-5"
+        className="container border border-primary rounded-3 d-flex flex-column justify-content-start mt-5"
       >
         <header id='main-container-header'>
-          <h1 className='text-center'>Random Password Generator</h1>
+          <h1 className='text-center fs-2'>Random Password Generator</h1>
         </header>
         <main id='main-container-body' className='d-flex align-items-center justify-content-center flex-grow-1'>
 
